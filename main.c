cb689343fe3404ed27b4f586c402fe4b17208ec8
uriellopes <uriel_ll@hotmail.com>
2016-11-29 13:46:51 -0200

    Update main.c

--- main.c
+++ main.c
@@ -53,29 +53,30 @@ void buscarFabricante(Produto *produto) { //Função para buscar produtos por fa
 
 void buscarPreco(Produto *produto){  //Funcao para ordenar os produtos do menor para o maior preço
     int i,j;
-    float aux;
+    Produto aux;
     for(i = 0; i < 10; i++) { //Ordenando os produtos de acordo com o preço
         for(j = i + 1; j < 10; j++) {
             if(produto[i].preco > produto[j].preco){
-                aux = produto[i].preco;
-                produto[i].preco = produto[j].preco;
-                produto[j].preco = aux;
+                aux = produto[i];
+                produto[i] = produto[j];
+                produto[j] = aux;
             }
         }
     }
     for(i = 0; i < 10; i++) { //Mostrando os produtos ja ordenados de acordo com o preço
-        printf("Produto: %s Preço: R$%.2 \n",produto[i].nome,produto[i].preco);
+        printf("Produto: %s Preço: R$%.2f \n",produto[i].nome,produto[i].preco);
     }
 }
 
 void buscarQtdEstoque(Produto *produto) { //Funcao para ordenar os produtos da menor para a maior quantidade no estoque
-    int i,j,aux;
+    int i,j;
+    Produto aux;
     for(i = 0; i < 10; i++) { //Ordenando os produtos de acordo com a quantidade no estoque
         for(j = i + 1; j < 10; j++) {
             if(produto[i].qtdestoque > produto[j].qtdestoque) {
-                aux = produto[i].qtdestoque;
-                produto[i].qtdestoque = produto[j].qtdestoque;
-                produto[j].qtdestoque = aux;
+                aux = produto[i];
+                produto[i] = produto[j];
+                produto[j] = aux;
             }
         }
     }
@@ -86,13 +87,13 @@ void buscarQtdEstoque(Produto *produto) { //Funcao para ordenar os produtos da m
 
 void ordemAlfabetica(Produto *produto) { //Função para ordenar os produtos em ordem alfabetica
     int i,j;
-    char aux[50];
+    Produto aux;
     for(i = 0; i < 10; i++) { // Ordenando os produtos
         for(j = 0; j < 10; j++) {
             if(strcmp(produto[i].nome,produto[j].nome) > 0) {
-                strcpy(aux,produto[i].nome);
-                strcpy(produto[i].nome,produto[j].nome);
-                strcpy(produto[j].nome,aux);
+                aux = produto[i];
+                produto[i] = produto[j];
+                produto[j] = aux;
             }
         }
     }
@@ -102,13 +103,14 @@ void ordemAlfabetica(Produto *produto) { //Função para ordenar os produtos em 
 }
 
 void ordenarCategoria(Produto *produto) { //Função para ordenar os produtos por categoria
-    int i,j,aux;
+    int i,j;
+    Produto aux;
     for(i = 0; i < 10; i++) { //Ordenando os produtos por categoria
         for(j = 0; j < 10; j++) {
             if(produto[i].categoria > produto[j].categoria) {
-                aux = produto[i].categoria;
-                produto[i].categoria = produto[j].categoria;
-                produto[j].categoria = aux;
+                aux = produto[i];
+                produto[i] = produto[j];
+                produto[j] = aux;
             }
         }
     }
@@ -151,7 +153,7 @@ void menuBuscar(Produto *produto) { //Menu de busca
 void carrinhoDeCompras(Produto *produto){ //Funcao do Carrinho de Compras
     Carrinho carrinho[10];
     char nomeProduto[50];
-    int escolha,j,i;
+    int escolha,j,i,naoExiste;
     float subtotal;
     for(i = 0; i < 10; i++) { //Limpando o carrinho
         carrinho[i].preco = 0;
@@ -187,6 +189,7 @@ void carrinhoDeCompras(Produto *produto){ //Funcao do Carrinho de Compras
                 if(carrinho[i].preco == 0) {
                     printf("Digite o nome do produto desejado: \n"); //Pegando o nome do produto que quer inserir no carrinho
                     scanf("%s",nomeProduto);
+                    naoExiste = 0;
                     for(j = 0; j < 10; j++) { //Verificando se o produto existe no registro
                         if(strcmp(nomeProduto,produto[j].nome) == 0){
                             strcpy(carrinho[i].nome,nomeProduto); //Salvando produto na posição vazia que foi encontrada
@@ -194,6 +197,11 @@ void carrinhoDeCompras(Produto *produto){ //Funcao do Carrinho de Compras
                             scanf("%i",carrinho[i].quantidade);
                             carrinho[i].preco = produto[j].preco * carrinho[i].quantidade; //Salvando o preço ja multiplicado pela quantidade do produto
                             break; //Parando o for para ele não continuar armazenando nos espaços vazios
+                        }else {
+                            naoExiste = naoExiste + 1; //Contador para saber se o produto existe no registro
+                        }
+                        if(naoExiste == 10) {
+                            printf("O produto não existe no mercado\n"); // Se o produto não existir exibira uma mensagem dizendo que não tem no registro
                         }
                     }
                     break; //Parando o outro for para não continuar armazenando nos espaços vazios
@@ -203,17 +211,28 @@ void carrinhoDeCompras(Produto *produto){ //Funcao do Carrinho de Compras
         case 2:
             printf("Digite o nome do produto que deseja remover: \n"); //Pegando o produto para remover
             scanf("%s",nomeProduto);
+            naoExiste = 0;
             for(i = 0; i < 10; i++) { //Verificando se o produto está no carrinho
                 if(strcmp(nomeProduto,carrinho[i].nome) == 0) { //Confirmando se ele está no carrinho e deletando caso esteja
                     strcpy(carrinho[i].nome,"0");
                     carrinho[i].preco = 0;
                     carrinho[i].quantidade = 0;
+                }else {
+                    naoExiste = naoExiste + 1; //Contador para saber se o produto existe no carrinho
                 }
+            }
+            if(naoExiste == 10) {
+                printf("O produto não está no carrinho\n"); //Se o produto não estiver no carrinho irá exibir uma mensagem avisando
             }
             break;
         case 3:
-            printf("Compra realizada com sucesso\n"); //Fechando as compras e mostrando o valor total
-            printf("TOTAL: R$%.2f",subtotal);
+            printf("Compra realizada com sucesso\n"); //Fechando as compras e mostrando o valor total e o que foi comprado
+            for(i = 0; i < 10; i++){
+                if(carrinho[i].preco != 0) {
+                    printf("Produto: %s Quantidade: %i Preço: %.2f\n",carrinho[i].nome,carrinho[i].quantidade,carrinho[i].preco);
+                }
+            }
+            printf("TOTAL: R$%.2f\n",subtotal);  //Valor total dos produtos que estão no carrinho
             break;
         case 0:
             break;
@@ -246,7 +265,100 @@ void menuCliente(Produto *produto){ //Função do menu do cliente
     }while(escolha != 0);
 }
 
-void menuFuncionario(){ //Função do menu do funcionario
+void reporEstoque(Produto *produto) { //Função para repor o estoque dos produtos
+    int i,naoExiste,novoEstoque;
+    char nomeProduto[50];
+    printf("Digite o produto que deseja repor: \n");  //Pegando o nome do produto que deseja repor
+    scanf("%s",nomeProduto);
+    naoExiste = 0;
+    for(i = 0; i < 10; i++) { //Verificando se ele existe no estoque, se não, aumentar o contador de que ele não existe
+            if(strcmp(nomeProduto,produto[i].nome) == 0){
+                printf("Digite a quantas unidades deseja adicionar ao estoque: \n");
+                scanf("%i",&novoEstoque);
+                produto[i].qtdestoque = produto[i].qtdestoque + novoEstoque;
+            }else {
+                naoExiste = naoExiste + 1;
+            }
+            if(naoExiste == 10) {
+                printf("O produto não existe no estoque"); //Caso o produto não exista, exibir a mensagem avisando
+            }
+    }
+}
+
+void inserirProduto(Produto *produto) { //Função para inserir novos produtos no registro
+    int i,registroCheio = 0;
+    for(i = 0; i < 10; i++) { //Verificando se tem algum lugar livre para adicionar produto novo, se não tiver , aumentar o contador
+        if(produto[i].qtdestoque == 0) { //Caso tenho um lugar livre, receber informações do produto
+            printf("Digite o nome do novo produto: \n");
+            scanf("%s",produto[i].nome);
+            printf("Digite o preço do novo produto: \n");
+            scanf("%f",&produto[i].preco);
+            printf("Digite o codigo do novo produto: \n");
+            scanf("%i",&produto[i].codigo);
+            printf("Digite o numero referente a categoria do produto: \n");
+            printf("1-Limepeza  2-Alimento Perecível  3-Alimento Não Perecível  4-Eletrônico\n");
+            scanf("%i",&produto[i].categoria);
+            printf("Digite a descrição do produto: \n");
+            scanf("%s",produto[i].descricao);
+            printf("Digite o nome do fabricante:\n");
+            scanf("%s",produto[i].fabricante);
+            printf("Digite a data de validade(DD MM AAAA):\n");
+            scanf("%i %i %i",&produto[i].validade.dia,&produto[i].validade.mes,&produto[i].validade.ano);
+            printf("Digite a quantidade do estoque:\n");
+            scanf("%i",&produto[i].qtdestoque);
+            printf("Digite o nº do corredor e o da prateleira onde localizar o produto:\n");
+            scanf("%i %i",&produto[i].corredor,&produto[i].prateleira);
+            break;
+        }else {
+            registroCheio = registroCheio + 1;
+        }
+        if(registroCheio == 10) {
+            printf("O mercado está cheio, retire algum produto para adicionar um novo");  //Se o registro estiver cheio, mostrar mensagem avisando
+        }
+    }
+}
+
+void menuFuncionario(Produto *produto){ //Função do menu do funcionario
+    int i,dia,mes,ano,escolha;
+
+    printf("Digite o dia mes e ano(DD MM AAAA): \n"); //Recebendo a data atual
+    scanf("%i %i %i",&dia,&mes,&ano);
+    for(i = 0; i < 10; i++) { //Pecorrendo o registro para saber os produtos que estão com o estoque baixo ou fora da validade
+        if(produto[i].qtdestoque < 20) { //Verificando quais produtos estão com o estoque baixo
+            printf("O produto %s está com o estoque baixo. Apenas %i unidades\n",produto[i].nome,produto[i].qtdestoque);
+        }
+        if(produto[i].validade.ano < ano) { // Verificando a validade dos produtos
+            printf("O produto %s está fora da validade\n",produto[i].nome);
+        }else if(produto[i].validade.mes < mes) {
+            printf("O produto %s está fora da validade\n",produto[i].nome);
+        }else if(produto[i].validade.dia < dia){
+            printf("O produto %s está fora da validade\n",produto[i].nome);
+        }
+    }
+    do {
+        printf("Escolha uma das opções: \n");
+        printf("1 - Inserir novos produtos\n");
+        printf("2 - Repor estoque\n");
+        printf("3 - Buscar produto\n");
+        printf("0 - Sair\n");
+        scanf("%i",&escolha);
+        switch(escolha) {
+        case 1:
+            inserirProduto(produto); //Chamando a função para inserir novos produtos
+            break;
+        case 2:
+            reporEstoque(produto); //Chamando a função para repor estoque
+            break;
+        case 3:
+            menuBuscar(produto); //Chamando a função do menu de busca
+            break;
+        case 0:
+            break;
+        default:
+            printf("Escolha uma das opções disponiveis\n");
+        }
+
+    }while(escolha != 0);
 }
 
 int main()
@@ -258,17 +370,20 @@ int main()
         printf("Erro na abertura do arquivo\n");
     } else {
         for(i = 0;i < 10; i++) { //Pegando as informações ja salvas no arquivo
-            fscanf(arquivo,"%s",produto[i].nome);
-            fscanf(arquivo,"%f",&produto[i].preco);
-            fscanf(arquivo,"%i",&produto[i].qtdestoque);
-            fscanf(arquivo,"%i",&produto[i].validade.dia);
-            fscanf(arquivo,"%i",&produto[i].validade.mes);
-            fscanf(arquivo,"%i",&produto[i].validade.ano);
-            fscanf(arquivo,"%s",produto[i].fabricante);
-            fscanf(arquivo,"%i",produto[i].categoria);
+            fscanf(arquivo,"%s\n",produto[i].nome);
+            fscanf(arquivo,"%i\n",&produto[i].categoria);
+            fscanf(arquivo,"%i\n",&produto[i].codigo);
+            fscanf(arquivo,"%f\n",&produto[i].preco);
+            fscanf(arquivo,"%s\n",produto[i].descricao);
+            fscanf(arquivo,"%s\n",produto[i].fabricante);
+            fscanf(arquivo,"%i %i %i\n",&produto[i].validade.dia,&produto[i].validade.mes,&produto[i].validade.ano);
+            fscanf(arquivo,"%i\n",&produto[i].corredor);
+            fscanf(arquivo,"%i\n",&produto[i].prateleira);
+            fscanf(arquivo,"%i\n",&produto[i].qtdestoque);
         }
     }
-    fclose(arquivo);
+    fclose(arquivo); //Fechando arquivo
+
     do { // Escolhendo entre o menu de cliente e funcionario
         printf("Escolha uma opção: \n");
         printf("1 - Cliente\n");
@@ -280,7 +395,7 @@ int main()
             menuCliente(produto); //Chamando a função do menu do cliente
             break;
         case 2:
-            menuFuncionario(); //Chamando a função do menu do funcionario
+            menuFuncionario(produto); //Chamando a função do menu do funcionario
             break;
         case 0:
             break;
@@ -289,5 +404,25 @@ int main()
         }
     }while (escolha != 0);
 
+    arquivo = fopen("produtos.txt","w"); //Abrindo arquivo para escrita
+    if(arquivo == NULL) { //Verificando se o arquivo abriu corretamente
+        printf("Erro na abertura do arquivo\n");
+    }else {
+        for(i = 0; i < 10; i++) { //Gravando os dados no arquivo
+            fprintf(arquivo,"%s\n",produto[i].nome);
+            fprintf(arquivo,"%i\n",produto[i].categoria);
+            fprintf(arquivo,"%i\n",produto[i].codigo);
+            fprintf(arquivo,"%f\n",produto[i].preco);
+            fprintf(arquivo,"%s\n",produto[i].descricao);
+            fprintf(arquivo,"%s\n",produto[i].fabricante);
+            fprintf(arquivo,"%i %i %i\n",produto[i].validade.dia,produto[i].validade.mes,produto[i].validade.ano);
+            fprintf(arquivo,"%i\n",produto[i].corredor);
+            fprintf(arquivo,"%i\n",produto[i].prateleira);
+            fprintf(arquivo,"%i\n",produto[i].qtdestoque);
+        }
+    }
+    fclose(arquivo); //Fechando arquivo com os novos dados salvos
+
     return 0;
 }
+
